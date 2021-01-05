09b9270b32f0739b3b7f70cae6b88d15ae8a440d
Fauzan Aji Prayoga <fauzanjr1@gmail.com>
2021-01-05 21:16:24 +0700

    Pertemuan 13 - Location Based Service

---
+++ LBSFauzan_11330/app/src/main/java/com/udinus/lbsfauzan_11330/MapsActivity.java
@@ -0,0 +1,49 @@
+package com.udinus.lbsfauzan_11330;
+
+import androidx.fragment.app.FragmentActivity;
+
+import android.os.Bundle;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+
+    private GoogleMap mMap;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_maps);
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+
+    /**
+     * Manipulates the map once available.
+     * This callback is triggered when the map is ready to be used.
+     * This is where we can add markers or lines, add listeners or move the camera. In this case,
+     * we just add a marker near Sydney, Australia.
+     * If Google Play services is not installed on the device, the user will be prompted to install
+     * it inside the SupportMapFragment. This method will only be triggered once the user has
+     * installed Google Play services and returned to the app.
+     */
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+        // Posisi Udinus
+        LatLng place = new LatLng(-6.982859, 110.409089);
+        float zoomLevel = 17.0f;
+
+        mMap.setMapType(GoogleMap.MAP_TYPE_HYBRID);
+        mMap.addMarker(new MarkerOptions().position(place).title("Universitas Dian NUswantoro Semarang"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(place,zoomLevel));
+    }
+}
